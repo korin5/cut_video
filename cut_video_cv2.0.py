669951bcd9476bad6106ac246a91b69e43f77557
Korin <1878464932@qq.com>
2024-01-05 11:10:29 +0800

    Update cut_video_cv2.0.py

--- cut_video_cv2.0.py
+++ cut_video_cv2.0.py
@@ -2,20 +2,18 @@ import cv2
 import subprocess
 import os
 
-# 输入的视频需要是连续的.mp4或.mkv文件
-input_video_dir = './20231216_171535_Video'
-output_video_dir = './20231216_171535_Video_Output2'
-id_start = 608  # 第一个视频第一帧的id（左上角显示）
+# 输入的视频需要是连续的.mp4或.mkv或.h264或.h265文件
+input_video_dir = './20240102/直线测试/video（三车道）'
+output_video_dir = './20240102_Output'
+id_start = 3847  # 第一个视频第一帧的id（左上角显示）
 id_cut = [      # 视频切片的id范围列表
-    [9056,10150],
-    [11733,12745],
-    [13797,14856],
-    [34943,35789],
-    [36901,37688]
+    [4042,4750],
+    [4971,5500],
+    [5870,6450],
 ]
 
 def cut_video(input_video_dir:str,output_video_dir:str,id_start:int,id_cut:list[list[int]]):
-    video_files = [os.path.join(input_video_dir, f) for f in os.listdir(input_video_dir) if f.endswith(('.mp4','.mkv'))]
+    video_files = [os.path.join(input_video_dir, f) for f in os.listdir(input_video_dir) if f.endswith(('.mp4','.mkv','h264','h265'))]
     if len(video_files) == 0:
         print("没有找到视频文件")
         exit()
@@ -27,9 +25,9 @@ def cut_video(input_video_dir:str,output_video_dir:str,id_start:int,id_cut:list[
     video.release()
 
     outputs = []
-    fourcc = cv2.VideoWriter.fourcc(*'MJPG')
+    fourcc = cv2.VideoWriter.fourcc(*'H264')
     for i in range(len(id_cut)):
-        outputs.append(cv2.VideoWriter(f"{output_video_dir}/{id_cut[i][0]}-{id_cut[i][1]}.mp4", fourcc, fps, (width, height)))
+        outputs.append(cv2.VideoWriter(f"{output_video_dir}/{id_cut[i][0]}-{id_cut[i][1]}.mkv", fourcc, fps, (width, height)))
 
     current_id = id_start
     for file in video_files:
@@ -42,9 +40,9 @@ def cut_video(input_video_dir:str,output_video_dir:str,id_start:int,id_cut:list[
                 if id_cut[i][0] <= current_id <= id_cut[i][1]:
                     outputs[i].write(frame)
                 if current_id == id_cut[i][1]+1:
-                    print("done: "+f"{id_cut[i][0]}-{id_cut[i][1]}.mp4")
+                    print("done: "+f"{id_cut[i][0]}-{id_cut[i][1]}.mkv")
                     outputs[i].release()
-                if id_cut[-1][1]+1 == current_id: return
+                if id_cut[-1][1]+3 == current_id: return
 
             current_id += 1
 
@@ -58,8 +56,13 @@ def to_HEVC(video_dir:str):
         command = f"ffmpeg -i {file} -map 0 -c:v libx264 -c:a copy -b:v 10M {file}_HEVC.mp4"
         print("run: " + command)
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        os.remove(file)
+        print(result)
+        
+    do_remove =  input("remove old output? [y|n]\n")
+    if do_remove == 'y':
+        for file in video_files:
+            os.remove(file)
 
 if __name__ == "__main__":
     cut_video(input_video_dir,output_video_dir,id_start,id_cut)
-    to_HEVC(output_video_dir)
+    # to_HEVC(output_video_dir)
